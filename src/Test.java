c7b033171ea3925e1aa814a226f2cc62fe897655
Bingjun Zhang <bingjunzhang@gmail.com>
2011-08-25 22:16:47 +0000

    Added the Test class

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+
+/**
+ * Created by bingjun at 8/25/11 5:08 PM
+ */
+public class Test {
+    public static void main(String[] args) {
+        int randomInt = Rand.getInt(10);
+        System.out.println(randomInt);
+    }
+}
