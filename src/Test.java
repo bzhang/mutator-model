d767ef0d2710fdb7200a40fe498e5b86138e6c18
Bingjun Zhang <bingjunzhang@gmail.com>
2012-01-19 13:58:15 -0600

    TST: test hashmap

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,19 @@
-import java.util.Random;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by bingjun at 8/25/11 5:08 PM
  */
 public class Test {
     public static void main(String[] args) {
-        int randomInt = Rand.getInt(10);
-        System.out.println(randomInt);
+        Map<String, ArrayList<String>> map = new HashMap<String, ArrayList<String>>();
+        ArrayList<String> mutationProperties = new ArrayList<String>();
+        mutationProperties.add("2");
+        mutationProperties.add("3.5");
+        map.put("1", mutationProperties);
+        System.out.println();
+        System.out.println("Elements of Map");
+        System.out.println(map);
     }
 }
