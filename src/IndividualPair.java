880d97b583999910c751dd9912746eec15a6dca1
Bingjun Zhang <bingjunzhang@gmail.com>
2012-01-19 16:38:01 -0600

    REF: pass currentGeneration, mutationMap, mutationProperties to mutate method

--- src/IndividualPair.java
+++ src/IndividualPair.java
@@ -23,10 +23,8 @@ public class IndividualPair {
     }
 
     public void mutate(int currentGeneration, Map mutationMap, Map mutationProperties) {
-        float mutatorStrengthA = getIndividualA().getMutatorStrength();
-        getIndividualA().mutate(currentGeneration, mutationMap, mutationProperties, mutatorStrengthA);
-        float mutatorStrengthB = getIndividualB().getMutatorStrength();
-        getIndividualB().mutate(currentGeneration, mutationMap, mutationProperties, mutatorStrengthB);
+        getIndividualA().mutate(currentGeneration, mutationMap, mutationProperties);
+        getIndividualB().mutate(currentGeneration, mutationMap, mutationProperties);
     }
 
     public IndividualPair reproduce() {
