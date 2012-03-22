1f737c56a6ab25395a2754bd1ff0608d1caa13f3
Bingjun Zhang <bingjunzhang@gmail.com>
2012-03-21 19:19:54 -0500

    ADD: new class IntegerPair.java

--- src/IntegerPair.java
+++ src/IntegerPair.java
@@ -1,7 +1,22 @@
 /**
- * @author bingjun
- *         Date: 3/21/12
- *         Time: 7:13 PM
+ * @author Bingjun Zhang
  */
+
 public class IntegerPair {
+    private int intA;
+    private int intB;
+
+    public IntegerPair(int intA, int intB) {
+        this.intA = intA;
+        this.intB = intB;
+    }
+
+    public int getIntA() {
+        return intA;
+    }
+
+    public int getIntB() {
+        return intB;
+    }
+
 }
