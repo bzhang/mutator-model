146ccc666e978ccf83525466bf943da8d8d9e33b
Bingjun Zhang <bingjunzhang@gmail.com>
2012-08-14 15:49:58 -0500

    MRG: solve conflicts in UnitTest.java

--- src/UnitTest.java
+++ src/UnitTest.java
@@ -5,20 +5,40 @@ import java.util.Arrays;
  */
 public class UnitTest {
     public static void main(String[] args) {
-        String propertiesFileName = args.length > 0 ? args[0] : "MutatorModel.properties";
-        ModelParameters.setPropertiesFileName(propertiesFileName);
+//        String propertiesFileName = args.length > 0 ? args[0] : "MutatorModel.properties";
+//        ModelParameters.setPropertiesFileName(propertiesFileName);
 
 //        float u = Rand.getFloat();
 //        double u2 = Rand.getDouble();
 //        System.out.println(u + "," + u2);
 
+//        testWeightedRandomGenerator();
 //        testPoisson();
 //        testMutatorEffect();
-        testFitnessEffect();
+//        testFitnessEffect();
+        testMersenneTwister();
+
+
+    }
+
+    private static void testMersenneTwister() {
+        int rep = 10;
+        for (int i = 0; i < rep; i++) {
+            double u = MersenneTwisterRand.getDouble();
+            System.out.println(u);
+        }
+    }
+
+    private static void testWeightedRandomGenerator() {
+        double[] totals = {1, 3, 5, 6, 10, 13, 30};
+        int index = Arrays.binarySearch(totals, 33);
+        index = (index >= 0) ? index : - index - 1;
+        System.out.println(index);
+
     }
 
     private static void testFitnessEffect() {
-        int rep = 100000;
+        int rep = 10000;
         double[] exponentialObs = new double[rep];
         for (int i = 0; i < rep; i++) {
             double u = Rand.getDouble();
@@ -28,21 +48,21 @@ public class UnitTest {
     }
 
     private static void testMutatorEffect() {
-        int rep = 100000;
+        int rep = 10000;
         double[] exponentialObs = new double[rep];
         for (int i = 0; i < rep; i++) {
-            exponentialObs[i] = Math.pow(Rand.getDouble(), ModelParameters.getFloat("MUTATOR_MUTATION_EFFECT"));
+            exponentialObs[i] = Math.pow(Rand.getDouble(), 0.03);
         }
-        System.out.println(Arrays.toString(exponentialObs));
+        Util.writeFile("test.txt", Arrays.toString(exponentialObs));
     }
 
     private static void testPoisson() {
-        int rep = 100000;
+        int rep = 10000;
         int[] poissonObs = new int[rep];
         for (int i = 0; i < rep; i++) {
-            poissonObs[i] = Util.getPoisson(1);
+            poissonObs[i] = Util.getPoisson(0.05);
         }
-        System.out.println(Arrays.toString(poissonObs));
+        Util.writeFile("test.txt", Arrays.toString(poissonObs));
     }
 
 }
