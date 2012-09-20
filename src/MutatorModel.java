b81563eaabfc2a01a8704fdabf4dbb43f381c790
Bingjun Zhang <bingjunzhang@gmail.com>
2012-09-20 14:45:34 -0500

    REF: remove unused methods

--- src/MutatorModel.java
+++ src/MutatorModel.java
@@ -106,30 +106,23 @@ public class MutatorModel {
         double[] fitnessArray = fitnessPropertiesArray.getFitnessArray();
         int[] nDeleMutArray = fitnessPropertiesArray.getNDeleMutArray();
         int[] nBeneMutArray = fitnessPropertiesArray.getnBeneMutArray();
-        double[] deleFitnessEffectArray = fitnessPropertiesArray.getDeleFitnessEffectArray();
-        double[] beneFitnessEffectArray = fitnessPropertiesArray.getBeneFitnessEffectArray();
-
-//        double[] fitnessArray = population.getFitnessArray();
+        double[] meanDeleFitnessEffectArray = fitnessPropertiesArray.getMeanDeleFitnessEffectArray();
+        double[] meanBeneFitnessEffectArray = fitnessPropertiesArray.getMeanBeneFitnessEffectArray();
         double[] mutatorStrengthArray = population.getMutatorStrengthArray();
-//        int[] nDeleMutArray = population.getNMutationsArray().getNDeleMutArray();
-//        int[] nBeneMutArray = population.getNMutationsArray().getnBeneMutArray();
-//        GroupReturn fitnessEffectArrayPair = population.getFitnessEffectArrayPair();
-//        double[] deleFitnessEffectArray = fitnessEffectArrayPair.getDeleFitnessEffectArray();
-//        double[] beneFitnessEffectArray = fitnessEffectArrayPair.getBeneFitnessEffectArray();
 
 
         return i + "\t" + Util.mean(fitnessArray)
                 + "\t" + Util.mean(mutatorStrengthArray)
                 + "\t" + Util.mean(nDeleMutArray)
                 + "\t" + Util.mean(nBeneMutArray)
-                + "\t" + Util.mean(deleFitnessEffectArray)
-                + "\t" + Util.mean(beneFitnessEffectArray)
+                + "\t" + Util.mean(meanDeleFitnessEffectArray)
+                + "\t" + Util.mean(meanBeneFitnessEffectArray)
                 + "\t" + Util.standardDeviation(fitnessArray)
                 + "\t" + Util.standardDeviation(mutatorStrengthArray)
                 + "\t" + Util.standardDeviation(nDeleMutArray)
                 + "\t" + Util.standardDeviation(nBeneMutArray)
-                + "\t" + Util.standardDeviation(deleFitnessEffectArray)
-                + "\t" + Util.standardDeviation(beneFitnessEffectArray)
+                + "\t" + Util.standardDeviation(meanDeleFitnessEffectArray)
+                + "\t" + Util.standardDeviation(meanBeneFitnessEffectArray)
                 + "\n";
     }
 
