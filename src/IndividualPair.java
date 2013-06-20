7ebe6d3fb053d908523d0a8668a86ac48a1ef161
Bingjun Zhang <bingjunzhang@gmail.com>
2013-06-20 16:20:37 -0500

    REF: calculate mutator strength before mutate()

--- src/IndividualPair.java
+++ src/IndividualPair.java
@@ -11,24 +11,38 @@ public class IndividualPair {
     public IndividualPair(Individual individualA, Individual individualB) {
         this.individualA = individualA;
         this.individualB = individualB;
+//        this.individualA.calculateMutatorStrength();
+//        this.individualB.calculateMutatorStrength();
     }
 
     public Individual getIndividualA() {
+//        individualA.calculateMutatorStrength();
         return individualA;
     }
 
     public Individual getIndividualB() {
+//        individualB.calculateMutatorStrength();
         return individualB;
     }
 
     public void mutate(int currentGeneration, ArrayList mutationProperties, double parentFitnessMean, double parentFitnessSD, double corFitnessMutatorStrength) {
+        getIndividualA().calculateMutatorStrength();
+        getIndividualB().calculateMutatorStrength();
         getIndividualA().mutate(currentGeneration, mutationProperties, parentFitnessMean, parentFitnessSD, corFitnessMutatorStrength);
         getIndividualB().mutate(currentGeneration, mutationProperties, parentFitnessMean, parentFitnessSD, corFitnessMutatorStrength);
     }
 
     public void mutate(int currentGeneration) {
+        getIndividualA().calculateMutatorStrength();
+        getIndividualB().calculateMutatorStrength();
+//        System.out.println("before mutate = " + getIndividualA().getMutatorStrength());
+//        System.out.println("before mutate = " + getIndividualB().getMutatorStrength());
         getIndividualA().mutate(currentGeneration);
         getIndividualB().mutate(currentGeneration);
+//        getIndividualA().calculateMutatorStrength();
+//        getIndividualB().calculateMutatorStrength();
+//        System.out.println("after mutate = " + getIndividualA().getMutatorStrength());
+//        System.out.println("after mutate = " + getIndividualB().getMutatorStrength());
     }
 
     public IndividualPair reproduce() {
