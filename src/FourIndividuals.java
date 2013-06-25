3701e357770646710f638a0557aac990dcd7b79c
Bingjun Zhang <bingjunzhang@gmail.com>
2013-06-24 19:44:46 -0500

    ADD: FourIndividuals and getters

--- src/FourIndividuals.java
+++ src/FourIndividuals.java
@@ -2,4 +2,32 @@
  @author Bingjun Zhang
  */
 public class FourIndividuals {
+    private IndividualPair individualPairA, individualPairB;
+    private Individual individualA, individualB, individualC, individualD;
+
+    public FourIndividuals(Individual individualA, Individual individualB, Individual individualC, Individual individualD) {
+        this.individualA = individualA;
+        this.individualB = individualB;
+        this.individualC = individualC;
+        this.individualD = individualD;
+    }
+
+    public Individual getIndividual(int i) {
+        Individual newIndividual = null;
+        switch (i) {
+            case 1:
+                newIndividual = individualA;
+                break;
+            case 2:
+                newIndividual = individualB;
+                break;
+            case 3:
+                newIndividual = individualC;
+                break;
+            case 4:
+                newIndividual = individualD;
+                break;
+        }
+        return newIndividual;
+    }
 }
