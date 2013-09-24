6c9c80ea7b4699c4c1651c471cf4b3ab7e567e38
Bingjun Zhang <bingjunzhang@gmail.com>
2013-09-24 18:40:31 -0500

    TST: remove ScatterAdd.java, add XYZDemo1.java for testing

---
+++ src/XYZDemo1.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: bingjun
+ * Date: 9/24/13
+ * Time: 11:53 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class XYZDemo1 {
+}
